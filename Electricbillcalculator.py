69e01b67ff6074bd8ac5dcc10a6450a8821830de
sam-2108 <okochibwire296@gmailcom>
2026-03-02 21:04:50 +0300

    version 1

---
+++ Electricbillcalculator.py
@@ -0,0 +1,7 @@
+while True:
+    powe=float(input("Enter your power consumption per hour (Kwh): "))
+    time=float(input("Enter the duration of power usage per day: "))
+    tax= 250
+    print("""
+
+    """)
