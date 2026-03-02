24363bb37a4295927603835b100dced22b8a31d2
sam-2108 <okochibwire296@gmailcom>
2026-03-03 02:04:28 +0300

    Version 1

---
+++ Grading.py
@@ -0,0 +1,12 @@
+Name=input("Enter your name: ")
+Marks=int(input("Enter your marks: "))
+if Marks>100:
+    print("Max mus be 0-100")
+elif Marks>=80:
+    print("A")
+elif Marks>=60:
+    print("B")
+elif Marks >=40:
+    print("C")
+else:
+    print("D")
