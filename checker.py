a018b4e17f8524ad297f47c11ce360808f2a2ed2
sam-2108 <okochibwire296@gmailcom>
2026-03-02 20:59:28 +0300

    version 1

---
+++ checker.py
@@ -0,0 +1,9 @@
+print("This program checks what type of number you have just keyed")
+number= int(input("Enter your number"))
+if number>0:
+    print("You just entered a positive number.")
+
+elif number<0:
+    print("This number you have entered is a negative number.")
+else:
+    print("This is a zero")
