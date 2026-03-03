9df7de7c0d6d929fcf768fe973e06af4ffe217bc
sam-2108 <okochibwire296@gmailcom>
2026-03-03 10:27:43 +0300

    Version 1

---
+++ Name.py
@@ -0,0 +1,11 @@
+name=input("Welcome please enter your name: ")
+age=int(input("Please enter your age too: "))
+age +=1
+
+print(f"Hello {name}, you will be {age} next year")
+
+marks= 50
+print(f"Your marks is {marks}")
+marks= 70
+print(f"Your new mark is {marks}")
+
