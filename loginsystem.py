f62a61d1c6be94cd77fb4f1090d5f534ac6c8092
sam-2108 <okochibwire296@gmailcom>
2026-03-03 10:25:11 +0300

    Version 1

---
+++ loginsystem.py
@@ -0,0 +1,12 @@
+username=""
+password=""
+while not username:
+    username=input("Enter your username: ")
+while not password:
+    password=input("Enter your password: ")
+
+if username =="Jack" and password =="prey":
+    print("welcome")
+else:
+    print("One of the credentials is missing.")
+
