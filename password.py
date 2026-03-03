99aa0c43f960f4e482dc52ebf43e63eefa181141
sam-2108 <okochibwire296@gmailcom>
2026-03-03 10:32:13 +0300

    Version 1

---
+++ password.py
@@ -0,0 +1,8 @@
+password="Elephant"
+response=0
+
+while response!= password:
+    response = input("Enter your password: ")
+    print("Enter a valid password!")
+else:
+    print("Welcome back user")
