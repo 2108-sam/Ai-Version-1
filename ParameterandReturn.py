a67423a274644e8196f560425d267dbe8f3ebb5e
sam-2108 <okochibwire296@gmailcom>
2026-03-03 10:31:23 +0300

    Version 1

---
+++ ParameterandReturn.py
@@ -0,0 +1,26 @@
+#Return and Receive
+#Demonstrate parameters and return values
+
+def display(message):
+    print(message)
+
+def give_me_five():
+    five=5
+    return five
+
+def ask_yes_no(question):
+    """Ask a yes or no question."""
+    response=None
+    while response not in("y", "n"):
+        response= input(question).lower()
+    return response
+#main
+display("Here's a message for you.\n")
+
+number=give_me_five()
+print(f"Here's what i go from give_me_five: {number}")
+
+answer= ask_yes_no("\nPlease enter 'y' or 'n': ")
+print(f"Thanks for entering: {answer}")
+
+input("\n\nPress the enter key to exit")
